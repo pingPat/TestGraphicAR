dc64123e40d50af316dfd9a478bf822e7abae614
pingPat <patpat1993@gmail.com>
2015-02-10 22:06:59 +0700

    Add Canvas+location
    *Graphic Bug

---
+++ TestGraphic/src/com/example/testgraphic/Point.java
@@ -0,0 +1,16 @@
+package com.example.testgraphic;
+
+public class Point {
+
+	public double longitude = 0f;
+    public double latitude = 0f;
+    public String description;
+    public float x, y = 0;
+
+    public Point(double lat, double lon, String desc) {
+        this.latitude = lat;
+        this.longitude = lon;
+        this.description = desc;
+    }
+	
+}
